38db860c9d0f5d4666d59106eb9923f36ea9632d
Muhammed Aslam TS <taslam8797@gmail.com>
2023-11-03 11:59:32 +0530

    botton

---
+++ client/src/components/Projects.tsx
@@ -0,0 +1,119 @@
+const Projects = () => {
+  return (
+    <div className="pt-10">
+      <h1 className="flex justify-center md:mt-16 text-2xl font-bold">
+        OUR PROJECTS
+      </h1>
+      <div>
+        <div className="flex flex-wrap pt-16 lg:pt-28 items-center justify-center gap-7">
+          <div className="w-full sm:w-1/2 pb-16 md:w-1/3 lg:w-1/4 xl:w-1/5 p-2">
+            <div className="relative flex w-full flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+              <div className="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r bg-gray-400 "></div>
+              <div className="p-6">
+                <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                  Tailwind card
+                </h5>
+                <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                  felis ligula.
+                </p>
+              </div>
+              <div className="p-6 pt-0">
+                <button
+                  data-ripple-light="true"
+                  type="button"
+                  className="select-none rounded-lg bg-black py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-500/20 transition-all hover:shadow-lg hover:shadow-gray-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                >
+                  Read More
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="w-full sm:w-1/2 md:w-1/3 pb-16 lg:w-1/4 xl:w-1/5 p-2">
+            <div className="relative flex w-full flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+              <div className="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r bg-gray-400 "></div>
+              <div className="p-6">
+                <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                  Tailwind card
+                </h5>
+                <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                  felis ligula.
+                </p>
+              </div>
+              <div className="p-6 pt-0">
+                <button
+                  data-ripple-light="true"
+                  type="button"
+                  className="select-none rounded-lg bg-black py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-gray-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                >
+                  Read More
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="w-full sm:w-1/2 pb-16 md:w-1/3 lg:w-1/4 xl:w-1/5 p-2">
+            <div className="relative flex w-full flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+              <div className="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r bg-gray-400 "></div>
+              <div className="p-6">
+                <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                  Tailwind card
+                </h5>
+                <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                  felis ligula.
+                </p>
+              </div>
+              <div className="p-6 pt-0">
+                <button
+                  data-ripple-light="true"
+                  type="button"
+                  className="select-none rounded-lg bg-black py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-gray-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                >
+                  Read More
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center">
+        <button className="hover:text-black hover:bg-white hover:border hover:border-black sm:w-auto w-full mt-8 m-7 text-base justify-between focus:outline-none font-medium leading-none text-white py-4 px-8 bg-gray-800 flex items-center">
+          More Projects
+          <div className="ml-2 mt-0.5">
+            <svg
+              className="fill-stroke"
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M3.33325 8H12.6666"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M10 10.6667L12.6667 8"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M10 5.33301L12.6667 7.99967"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
