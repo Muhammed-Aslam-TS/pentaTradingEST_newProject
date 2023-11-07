c65297376d37402e9a0c426b391115e69fcfc925
Muhammed Aslam TS <taslam8797@gmail.com>
2023-11-07 19:06:50 +0530

    aslam

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -13,7 +13,7 @@ const About = () => {
         }}
         className="md:flex w-full h-auto items-center justify-center gap-36"
       >
-        <div className="flex md:shadow-none sm:justify-center sm:shadow-2xl w-80 h-96 md:justify-end mt-7 ms-11 ">
+        <div className="flex sm:justify-center w-80 h-96 md:justify-end mt-7 ms-11 ">
           <img className="h-full w-full" src={image} alt="" srcSet="" />
         </div>
 
